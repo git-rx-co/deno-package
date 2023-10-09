d215c6b9188c28f022bd70e1ef7155bcc7d97301
Git RX <144759431+git-rx-co@users.noreply.github.com>
2023-10-09 18:02:21 +0600

    new up test.ts

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ function newfn ( ){
  return 10;
 }
 export function DenoTest(num:number) { 
-    let f = newfn();  
+    let f = newfn();   
     let res:number = (num * num) + f;
     return res  ; 
 }
